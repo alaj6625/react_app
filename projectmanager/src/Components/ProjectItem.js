55ad96ff3248bd8942561a6b6f31551798aa4e06
Alvin Allen <alaj6625@gmail.com>
2017-07-13 17:53:04 -0500

    added Project Item

---
+++ projectmanager/src/Components/ProjectItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+class ProjectItem extends Component {
+ 
+  render() {
+    console.log(this.props);
+    return (
+      <li className="Projects">
+        my Projects
+        
+      <li>
+    );
+  }
+}
+
+export default ProjectItem;
